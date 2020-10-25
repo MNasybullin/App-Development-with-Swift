dd2851a99c836eccdb716f5ad9db3c95a3608691
MNasybullin <mansurnasybullin@gmail.com>
2020-10-25 19:14:51 +0300

    Traffic segues proj

---
+++ TrafficSegues/TrafficSegues/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Stomach Diego on 10/25/20.
+//  Copyright © 2020 Mansur Nasybullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
+    
+}
+
