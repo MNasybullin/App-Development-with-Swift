799122ffeff97929dd77afd3a91f73cc7fcce652
MNasybullin <mansurnasybullin@gmail.com>
2020-10-26 20:13:59 +0300

    life cycle

---
+++ RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Stomach Diego on 10/25/20.
+//  Copyright © 2020 Mansur Nasybullin. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //tabBarItem.badgeValue = "!"
+    }
+
+
+}
+
---
+++ RainbowTabs/RainbowTabs/YellowViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YellowViewController.swift
+//  RainbowTabs
+//
+//  Created by Stomach Diego on 10/26/20.
+//  Copyright © 2020 Mansur Nasybullin. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
