9773a9b0b789a32b50a4b2c004b9a55b8a44f524
MNasybullin <mansurnasybullin@gmail.com>
2020-11-17 16:53:57 +0300

    fix

--- Working with the Web.playground/Contents.swift
+++ Working with the Web.playground/Contents.swift
@@ -30,36 +30,44 @@ struct PhotoInfo: Codable {
     var copyright: String?
     
     enum CodingKeys: String, CodingKey {
-        case title
-        case description = "explanation"
-        case url
-        case copyright
-    }
+            case title
+            case description = "explanation"
+            case url
+            case copyright
+        }
     
-    init(from decoder: Decoder) throws {
-        let valueContainer = try decoder.container(keyedBy: CodingKeys.self)
-        self.title = try valueContainer.decode(String.self, forKey: CodingKeys.title)
-        self.description = try valueContainer.decode(String.self, forKey: CodingKeys.description)
-        self.url = try valueContainer.decode(URL.self, forKey: CodingKeys.url)
-        self.copyright = try valueContainer.decode(String.self, forKey: CodingKeys.copyright)
+        init(from decoder: Decoder) throws {
+            let valueContainer = try decoder.container(keyedBy:
+               CodingKeys.self)
+            self.title = try valueContainer.decode(String.self,
+               forKey: CodingKeys.title)
+            self.description = try
+               valueContainer.decode(String.self, forKey:
+               CodingKeys.description)
+            self.url = try valueContainer.decode(URL.self, forKey:
+               CodingKeys.url)
+            self.copyright = try? valueContainer.decode(String.self,
+               forKey: CodingKeys.copyright)
+        }
     }
-}
-
 
 
 
 func fetchPhotoInfo(completion: @escaping (PhotoInfo?) -> Void) {
     let baseURL = URL(string: "https://api.nasa.gov/planetary/apod")!
+    
     let query: [String: String] = [
-        "api_key": "DEMO_KEY"
+        "api_key": "DEMO_KEY",
     ]
-
+    
     let url = baseURL.withQueries(query)!
-    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+    let task = URLSession.shared.dataTask(with: url) { (data,
+    response, error) in
         let jsonDecoder = JSONDecoder()
         if let data = data,
-            let photoInfo = try? jsonDecoder.decode(PhotoInfo.self, from: data) {
-            completion(photoInfo)
+            let photoInfo = try?
+              jsonDecoder.decode(PhotoInfo.self, from: data) {
+              completion(photoInfo)
         } else {
             print("Either no data was returned, or data was not properly decoded.")
             completion(nil)
@@ -67,6 +75,7 @@ func fetchPhotoInfo(completion: @escaping (PhotoInfo?) -> Void) {
     }
     task.resume()
 }
+
 
 fetchPhotoInfo { (fetchedInfo) in
     print(fetchedInfo)
